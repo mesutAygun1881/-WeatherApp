c697b8431fca0a473f62fb279fac3071b3b671ce
mesutAygun1881 <mesutaygun35@gmail.com>
2021-04-22 14:35:36 +0300

    initial

--- WeatherApp/DetailVC.swift
+++ WeatherApp/DetailVC.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class DetailVC: UIViewController {
-
+var city = String()
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        cityNameLabel.text = city
+        getTodayResult(cityNameLabel: city)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    func getTodayResult(cityNameLabel : String) {
+        
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityNameLabel)&appid=0f6112b1d663b03202ffabe9788c51ef") {
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request){(data,response,error) in
+                if error == nil {
+                    if let incomingData = data {
+                        do{
+                            let json = try JSONSerialization.jsonObject(with: incomingData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                            //print(json)
+                            if let main = json["main"] as? NSDictionary {
+                              if  let temp = main["temp"] as? Double {
+                                let state = Int(temp - 273.15)
+                                DispatchQueue.main.sync {
+                                    self.weatherLabel.text = String(state)
+                                }
+                                }
+                            }
+                        }catch{
+                            print("hata!!")
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonlabel: UIButton!
+    @IBOutlet weak var cityLabel: UITextField!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        buttonlabel.layer.cornerRadius = 10
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+        cityLabel.text = ""
     }
 
+    @IBAction func buttonClicked(_ sender: Any) {
+        let city = cityLabel.text ?? ""
+        
+        if city.isEmpty {
+            let alert = UIAlertController(title: "ERROR", message: "PLEASE TRY AGAIN", preferredStyle: UIAlertController.Style.alert)
+            let cancel = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(cancel)
+            self.present(alert, animated: true, completion: nil)
+        }else{
+            
+            let vc = self.storyboard?.instantiateViewController(identifier: "DetailViewController") as! DetailVC
+            vc.city = city
+            
+            self.show(vc, sender: nil)
+        }
+    
+}
 
 }
 
